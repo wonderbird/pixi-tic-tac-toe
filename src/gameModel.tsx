86561eb39e85dd22c2fe8bfa218520a046f7e070
Stefan Boos <kontakt@boos.systems>
2024-03-25 20:46:26 +0100

    feat: detect game over situation

--- src/gameModel.tsx
+++ src/gameModel.tsx
@@ -35,13 +35,40 @@ export class GameModel {
   }
 
   isGameOver() {
+    // check whether there is a row with the same symbol
     for (let row = 0; row < 3; row++) {
       if (this.board[row][0] !== '' && this.board[row][0] === this.board[row][1] && this.board[row][1] === this.board[row][2]) {
         return true;
       }
     }
-    
-    return false;
+
+    // check whether there is a column with the same symbol
+    for (let column = 0; column < 3; column++) {
+      if (this.board[0][column] !== '' && this.board[0][column] === this.board[1][column] && this.board[1][column] === this.board[2][column]) {
+        return true;
+      }
+    }
+
+    // check whether the diagonal from the top left to the bottom right has the same symbol
+    if (this.board[0][0] !== '' && this.board[0][0] === this.board[1][1] && this.board[1][1] === this.board[2][2]) {
+      return true;
+    }
+
+    // check whether the diagonal from the top right to the bottom left has the same symbol
+    if (this.board[0][2] !== '' && this.board[0][2] === this.board[1][1] && this.board[1][1] === this.board[2][0]) {
+      return true;
+    }
+
+    // check whether the board contains an empty square
+    for (let row = 0; row < 3; row++) {
+      for (let column = 0; column < 3; column++) {
+        if (this.board[row][column] === '') {
+          return false;
+        }
+      }
+    }
+
+    return true;
   }
 }
 
